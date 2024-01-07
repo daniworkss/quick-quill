8cde5be8d1c5906ce82123ce7a1417cacddcbe6f
daniworkss <daniworkss8@gmail.com>
2024-01-07 16:25:01 +0100

    http-to-https-correction

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return( 
        <>
+       <head>
+       <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+
+       </head>
        <Component {...pageProps} />
        {/* <Script src ="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js" /> */}
        <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></Script>

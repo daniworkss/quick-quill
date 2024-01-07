889f0106644bd48bebea408b83b66c7be71a4bc8
daniworkss <daniworkss8@gmail.com>
2024-01-07 16:26:23 +0100

    app-js-correction-1

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '@/styles/globals.css'
 import Script from 'next/script'
+import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return( 
        <>
-       <head>
-       <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
-
-       </head>
+       <Head>
+       <meta  httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+       </Head>
        <Component {...pageProps} />
        {/* <Script src ="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js" /> */}
        <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></Script>
